2abf8f2070579f7e69931d2818ffa585325f14f4
donatomarino <donato_8@icloud.com>
2025-02-25 20:49:27 +0100

    Toastify

--- app/client/src/components/LoginComponents/Header.js
+++ app/client/src/components/LoginComponents/Header.js
@@ -1,7 +1,7 @@
 export const Header = ({ onClick, description }) => {
     return (
         <div className='Login__HeaderContainer'>
-            <h1 className='Login__HeaderContainer--Title' onClick={onClick}>DMusic</h1>
+            <h1 className='Login__HeaderContainer--Title' onClick={onClick}>DM<span>usic</span></h1>
             <h2 className='Login__HeaderContainer--Description'>{description}</h2>
         </div>
     );
